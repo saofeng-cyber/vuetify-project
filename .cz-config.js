b1d97bcc3b57c40dcb2977a708846554633fc95e
sunminggang <942391953@qq.com>
2023-04-26 00:51:55 +0800

    ✨ feat(custom): 修改配置和功能

--- .cz-config.js
+++ .cz-config.js
@@ -1,36 +1,65 @@
 module.exports = {
   types: [
-    { value: "✨ feature", name: "✨ feature:  增加新功能" },
-    { value: "bug", name: "bug:      测试反馈bug列表中的bug号" },
-    { value: "fix", name: "fix:      修复bug" },
-    { value: "🚀 ui", name: "🚀 ui:       更新UI" },
-    { value: "docs", name: "docs:     文档变更" },
-    { value: "style", name: "style:    代码格式(不影响代码运行的变动)" },
-    { value: "perf", name: "perf:     性能优化" },
     {
-      value: "refactor",
-      name: "refactor: 重构(既不是增加feature，也不是修复bug)",
+      value: "✨ feat",
+      name: "✨ feat:     新功能",
     },
-    { value: "release", name: "release:  发布" },
-    { value: "deploy", name: "deploy:   部署" },
-    { value: "test", name: "test:     增加测试" },
     {
-      value: "chore",
-      name: "chore:    构建过程或辅助工具的变动(更改配置文件)",
+      value: ":bug: fix",
+      name: "🐛 fix:      修复bug",
     },
-    { value: "revert", name: "revert:   回退" },
-    { value: "build", name: "build:    打包" },
+    {
+      value: ":package: build",
+      name: "📦️ build:    打包",
+    },
+    {
+      value: ":zap: perf",
+      name: "⚡️ perf:     性能优化",
+    },
+    {
+      value: ":tada: release",
+      name: "🎉 release:  发布正式版",
+    },
+    {
+      value: ":lipstick: style",
+      name: "💄 style:    代码的样式美化",
+    },
+    {
+      value: ":recycle: refactor",
+      name: "♻️  refactor: 重构",
+    },
+    {
+      value: ":pencil2: docs",
+      name: "✏️  docs:     文档变更",
+    },
+    {
+      value: ":white_check_mark: test",
+      name: "✅ test:     测试",
+    },
+    {
+      value: ":rewind: revert",
+      name: "⏪️ revert:   回退",
+    },
+    {
+      value: ":rocket: chore",
+      name: "🚀 chore:    构建/工程依赖/工具",
+    },
+    {
+      value: ":construction_worker: ci",
+      name: "👷 ci:       CI related changes",
+    },
   ],
-  // override the messages, defaults are as follows
   messages: {
-    type: "请选择提交类型:",
-    customScope: "请输入您修改的范围(可选):",
-    subject: "请简要描述提交 message (必填):",
-    body: "请输入详细描述(可选，待优化去除，跳过即可):",
-    footer: "请输入要关闭的issue(待优化去除，跳过即可):",
-    confirmCommit: "确认使用以上信息提交？(y/n/e/h)",
+    type: "请选择提交类型(必填)",
+    customScope: "请输入文件修改范围(可选)",
+    subject: "请简要描述提交(必填)",
+    body: "请输入详细描述(可选)",
+    breaking: "列出任何BREAKING CHANGES(可选)",
+    footer: "请输入要关闭的issue(可选)",
+    confirmCommit: "确定提交此说明吗？",
   },
   allowCustomScopes: true,
+  // 跳过问题
   skipQuestions: ["body", "footer"],
   subjectLimit: 72,
 };
