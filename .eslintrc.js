7a59e33795a012da7add2da25642d235160a56c8
sunminggang <942391953@qq.com>
2023-04-25 23:28:04 +0800

    :sparkles: feat: 添加eslint和prettier校验文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-strongly-recommended',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    "plugin:vue/vue3-strongly-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  rules: {
-    'vue/multi-word-component-names': 'off',
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-}
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+};
